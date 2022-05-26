15f3075b7e1877bd44d33f3cd97adbeec5d9f6d6
hannagustinvega <hannheitiragustin@hotmail.com>
2022-05-26 18:52:31 -0300

    estructura con componentes botones sin interactividad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/Calculator_icon.png';
+import Boton from './components/Boton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Contenedor del logo */}
+      <div className='logo-container'>
+        <img 
+            className='logo'
+            src={logo}
+            alt='Icono de calculadora' />
+        <h1 className='logo-title'>Calculator</h1>
+        </div>
+        
+      {/* Contenedor principal */}
+      <div className='calculator-container'>
+        <div className='column'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='column'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='column'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='column'>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>=</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='column'></div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Boton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../stylesheets/Boton.css';
+
+function Button(props) {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+    /* isNaN(is not a number) esto devuelve true su ninguna de las sentencias da false, si una sola da false ya se considera false. */
+  };
+
+  return (
+    <div
+        className={`boton-container ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>{/* trimEnd te permite borrar espacios al final de una cadena de caracteres */}
+      {props.children}
+    </div>
+  );
+};
+
+export default Button;
