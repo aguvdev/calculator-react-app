c8ae2919c74476d3be3ee7eeb46cbf8aa1251ccb
hannagustinvega <hannheitiragustin@hotmail.com>
2022-05-30 20:36:28 -0300

    update: estructura con condicional

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -8,14 +8,25 @@ function Button(props) {
     /* isNaN(is not a number) esto devuelve true su ninguna de las sentencias da false, si una sola da false ya se considera false. */
   };
 
-  return (
-    <div
-        className={`boton-container ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
-        onClick={() => props.manejarClick(props.children)}>
-          {/* trimEnd te permite borrar espacios al final de una cadena de caracteres */}
-      {props.children}
-    </div>
-  );
+  if(esOperador(props.children)) {
+    return (
+      <div
+          className='boton-container operador'
+          onClick={() => props.manejarClick(props.children)}>
+            {/* trimEnd te permite borrar espacios al final de una cadena de caracteres */}
+        {props.children}
+      </div>
+    );
+  } else {
+    return (
+      <div
+          className='boton-container'
+          onClick={() => props.manejarClick(props.children)}>
+            {/* trimEnd te permite borrar espacios al final de una cadena de caracteres */}
+        {props.children}
+      </div>
+    );
+  }
 };
 
 export default Button;
